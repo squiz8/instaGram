a8de27544c5bd21526411dc76de52738d28720a5
squiz8 <sammy_squiz@yahoo.com>
2019-09-12 14:43:50 +0100

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
                    
                     <div><img src="/svg/instaGramLogo.svg" style="height:20px; border-right:1px solid #333" class="pr-3"></div>
-                    <div class="pl-3 pt-1">instaGram</div>
+                    <div class="pl-3 pt-1">robuloxGram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
